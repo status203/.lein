50f301202aca31da8062679b23df116ac5b76964
status203 <git@status203.com>
2012-06-27 13:49:38 +0100

    lein-midje and lein-swank working in lein2

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,3 @@
-{:user {:plugins [swank-clojure "1.4.2"]
-        [lein-midje "2.0.0-SNAPSHOT"]}}
+{:user {:plugins [[lein-midje "2.0.0-SNAPSHOT"]
+                  [lein-swank "1.4.4"]]
+        :dev-dependencies []}}

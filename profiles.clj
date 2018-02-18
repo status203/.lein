247e7aa7781cbbb7b0ab91190445f6fe41e59e78
Adam Gibbs <git@status203.ocm>
2018-02-18 16:33:24 +0000

    Updating prior to setting up lein-test-refresh

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,5 @@
-{:user {:plugins [[lein-midje "2.0.0-SNAPSHOT"]
-                  [lein-ritz "0.5.0"]]
-        :dev-dependencies []
-        }}
-
+{:user {:plugins      [[cider/cider-nrepl "0.11.0-SNAPSHOT"]
+                       [refactor-nrepl "2.0.0-SNAPSHOT"]]
+        :dependencies [[alembic "0.3.2"]
+                       [org.clojure/tools.nrepl "0.2.12"]]}}
+ 
